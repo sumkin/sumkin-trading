7362af44bf300fb2555ae2a969332de7822c9305
Fedor Nikitin <fedor.nikitin@gmail.com>
2024-08-16 01:30:28 +0300

    Finished implementing crossover strategy. Added sl and tp optimization.

--- src/data_reader.py
+++ src/data_reader.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 from alpaca.data.timeframe import TimeFrame
@@ -15,7 +16,7 @@ class DataReader:
     def __init__(self):
         self.client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-    def read_bars(self, symbol, timeframe, start, end):
+    def read_bars(self, ticker, timeframe, start, end):
         """
         Reads bars data.
         :param symbol: e.g. 'TSLA'
@@ -24,18 +25,30 @@ class DataReader:
         :param end: 'YYYY-MM-DD hh:mm:ss'
         :return: pandas Dataframe
         """
-        request_params = StockBarsRequest(symbol_or_symbols=[symbol],
-                                          timeframe=timeframe,
-                                          start=start,
-                                          end=end)
-        bars = self.client.get_stock_bars(request_params)
-        res_df = bars.df.reset_index()
-        res_df = res_df.rename(columns={"timestamp": "Time",
-                                        "open": "Open",
-                                        "high": "High",
-                                        "low": "Low",
-                                        "close": "Close",
-                                        "volume": "Volume"})
-        res_df["Time"] = pd.to_datetime(res_df["Time"])
-        res_df = res_df.set_index("Time")
+
+        # Check cache.
+        fname = "../data/{}_{}_{}_{}.csv.gz".format(ticker,
+                                                    start.replace(":", "").replace(" ", "_"),
+                                                    end.replace(":", "").replace(" ", "_"),
+                                                    str(timeframe))
+        if os.path.isfile(fname):
+            res_df = pd.read_csv(fname)
+            res_df["Time"] = pd.to_datetime(res_df["Time"])
+            res_df = res_df.set_index("Time")
+        else:
+            request_params = StockBarsRequest(symbol_or_symbols=[ticker],
+                                              timeframe=timeframe,
+                                              start=start,
+                                              end=end)
+            bars = self.client.get_stock_bars(request_params)
+            res_df = bars.df.reset_index()
+            res_df = res_df.rename(columns={"timestamp": "Time",
+                                            "open": "Open",
+                                            "high": "High",
+                                            "low": "Low",
+                                            "close": "Close",
+                                            "volume": "Volume"})
+            res_df["Time"] = pd.to_datetime(res_df["Time"])
+            res_df = res_df.set_index("Time")
+            res_df.to_csv(fname)
         return res_df
--- src/ma_crossover_strategy.py
+++ src/ma_crossover_strategy.py
@@ -1,36 +1,65 @@
+import talib
 from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 from backtesting import Backtest, Strategy
 from backtesting._util import _Array
 from datetime import datetime, timedelta
 
 from data_reader import DataReader
+from universe import Universe
 
 class MACrossoverStrategy(Strategy):
-    FAST_PERIOD = 20
-    SLOW_PERIOD = 50
+    FAST_PERIOD = 9
+    SLOW_PERIOD = 21
+    SL = 0.02
+    TP = 0.04
 
     def init(self):
         self.data.df["fast_ma"] = self.data.df["Close"].ewm(span=MACrossoverStrategy.FAST_PERIOD).mean().values
         self.data.df["slow_ma"] = self.data.df["Close"].ewm(span=MACrossoverStrategy.SLOW_PERIOD).mean().values
         self.fast_ma = self.I(lambda x: x, _Array(self.data.df["fast_ma"].values, name="fast_ma"))
         self.slow_ma = self.I(lambda x: x, _Array(self.data.df["slow_ma"].values, name="slow_ma"))
+        self.atr = self.I(lambda x: x,
+                          _Array(talib.ATR(self.data.df["High"].values,
+                                           self.data.df["Low"].values,
+                                           self.data.df["Close"].values,
+                                           timeperiod=20), name="atr"))
 
     def next(self):
         if self.fast_ma[-2] < self.slow_ma[-2] and self.fast_ma[-1] > self.slow_ma[-1]:
-            self.buy()
-        elif self.slow_ma[-2] > self.slow_ma[-2] and self.fast_ma[-1] < self.slow_ma[-1]:
-            self.sell()
+            # Long signal. Fast MA crosses slow MA from below to above.
+
+            # Closing price should be above fast MA.
+            if self.data.Close[-1] > self.fast_ma[-1]:
+                # Difference between fast MA and the slow MA is less than the ATR.
+                if self.fast_ma[-1] - self.slow_ma[-1] < self.atr[-1]:
+                    self.buy(sl=(1.0 - MACrossoverStrategy.SL) * self.data.Close[-1],
+                             tp=(1.0 + MACrossoverStrategy.TP)* self.data.Close[-1])
+        elif self.fast_ma[-2] > self.slow_ma[-2] and self.fast_ma[-1] < self.slow_ma[-1]:
+            # Short signal. Fast MA crosses slow MA from above to slow.
+
+            # Closing prices should be below fast MA.
+            if self.data.Close[-1] < self.fast_ma[-1]:
+                # Difference between fast MA and the slow MA is less than the ATR.
+                if self.fast_ma[-1] - self.slow_ma[-1] < self.atr[-1]:
+                    self.sell(sl=(1.0 + MACrossoverStrategy.TP) * self.data.Close[-1],
+                              tp=(1.0 - MACrossoverStrategy.SL) * self.data.Close[-1])
 
 if __name__ == "__main__":
     end = datetime.now() - timedelta(days=1)
-    start = (end - timedelta(days=14)).strftime("%Y-%m-%d %H:%M:%S")
+    start = (end - timedelta(days=365)).strftime("%Y-%m-%d %H:%M:%S")
     end = end.strftime("%Y-%m-%d %H:%M:%S")
 
-    dr = DataReader()
-    df = dr.read_bars("TSLA",
-                      TimeFrame(5, TimeFrameUnit.Minute),
-                      start,
-                      end)
-    bt = Backtest(df, MACrossoverStrategy)
-    bt.run()
-    bt.plot(filename="../output/ma_crossover.html")
+    tickers = Universe().get_tickers()
+    #tickers = ["ABBV"]
+    cum_win_rate = 0.0
+    for i, ticker in enumerate(tickers):
+        dr = DataReader()
+        df = dr.read_bars(ticker,
+                          TimeFrame(1, TimeFrameUnit.Day),
+                          start,
+                          end)
+        bt = Backtest(df, MACrossoverStrategy)
+        stats = bt.run()
+        win_rate = stats["Win Rate [%]"]
+        cum_win_rate += win_rate
+        print(i, ticker, win_rate, cum_win_rate / (i + 1))
