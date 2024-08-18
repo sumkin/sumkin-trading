9e1941a2f5c8b3c1457c1cf0d52bcbc2dcb8f94d
Fedor Nikitin <fedor.nikitin@gmail.com>
2024-08-18 22:26:28 +0300

    Added optimizaiton of fast and slow.

--- src/optimize_ma_crossover_strategy.py
+++ src/optimize_ma_crossover_strategy.py
@@ -46,5 +46,46 @@ def optimize_sl_tp():
     print("max_sl = {}".format(max_sl))
     print("max_tp = {}".format(max_tp))
 
+def optimize_fast_slow():
+    end = datetime(2024,8,14)
+    start = (end - timedelta(days=365)).strftime("%Y-%m-%d") + " 00:00:00"
+    end = end.strftime("%Y-%m-%d") + " 23:59:59"
+
+    tickers = Universe().get_tickers()
+
+    max_fast = None
+    max_slow = None
+    max_win_rate = -np.inf
+    for fast in np.arange(5, 50, 5):
+        for slow in np.arange(fast + 5, 100, 10):
+            def process(ticker):
+                dr = DataReader()
+                df = dr.read_bars(ticker,
+                                  TimeFrame(1, TimeFrameUnit.Day),
+                                  start,
+                                  end)
+                MACrossoverStrategy.FAST_PERIOD = fast
+                MACrossoverStrategy.SLOW_PERIOD = slow
+                bt = Backtest(df, MACrossoverStrategy)
+                stats = bt.run()
+                win_rate = stats["Win Rate [%]"]
+                if win_rate != win_rate:
+                    return None
+                else:
+                    return win_rate
+
+            with WorkerPool(n_jobs=cpu_count() - 1) as pool:
+                win_rates = pool.map(process, tickers)
+                win_rates = [e for e in win_rates if e is not None]
+
+            mean_win_rate = np.mean(win_rates)
+            if mean_win_rate > max_win_rate:
+                max_fast = fast
+                max_slow = slow
+                max_win_rate = mean_win_rate
+            print("{}, {}: Mean, std win rate = {}, {}".format(fast, slow, np.mean(win_rates), np.std(win_rates)))
+    print("max_fast = {}".format(max_fast))
+    print("max_slow = {}".format(max_slow))
+
 if __name__ == "__main__":
-    optimize_sl_tp()
+    optimize_fast_slow()
