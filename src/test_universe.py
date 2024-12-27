9327ff236372354f62170d4aeaedf584bb5cb3e8
Fedor Nikitin <fedor.nikitin@gmail.com>
2024-12-27 15:02:46 +0200

    Fixed tests.

--- src/test_universe.py
+++ src/test_universe.py
@@ -1,7 +1,12 @@
-from universe import Universe
+from tinkoff_universe import TinkoffUniverse
+from kraken_universe import KrakenUniverse
 
-def test_universe():
-    universe = Universe()
-    tickers = universe.get_tickers()
-    assert len(tickers) >= 500
+def test_tinkoff_universe():
+    tu = TinkoffUniverse()
+    tickers = tu.get_tickers()
+    assert len(tickers) >= 5 # There should be some tickers.
 
+def test_kraken_universe():
+    ku = KrakenUniverse()
+    tickers = ku.get_tickers()
+    assert len(tickers) >= 5 # There should be some tickers.
