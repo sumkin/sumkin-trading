5a4534774366ad30a713d11dd1e4042c3257b459
Fedor Nikitin <fedor.nikitin@gmail.com>
2025-06-28 17:34:43 +0300

    Added create_cca_database.py file.

--- src/cca/create_cca_database.py
+++ src/cca/create_cca_database.py
@@ -1,12 +1,22 @@
 import sqlite3
 
 def create_cca_database():
-    conn = sqlite3.connect("../data/cca_trades.db")
+    conn = sqlite3.connect("../../data/cca_trades.db")
     cursor = conn.cursor()
     q = '''
     CREATE TABLE IF NOT EXISTS cca_trades (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        
-    )
+        spot_ticker TEXT NOT NULL,
+        futures_ticker TEXT NOT NULL,
+        bid REAL,
+        ask REAL,
+        vol REAL,
+        enter_dt TEXT,
+        exit_dt TEXT)
     '''
+    cursor.execute(q)
+    conn.commit()
+
+if __name__ == "__main__":
+    create_cca_database()
 
